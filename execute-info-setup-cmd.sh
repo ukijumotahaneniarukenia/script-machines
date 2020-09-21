244b9b7c9b7a6a573ff37c133334c2164b580a2c
ukijumotahaneniarukenia <mrchildrenkh1008@gmail.com>
2020-09-21 20:17:15 +0900

    sketch

--- execute-info-setup-cmd.sh
+++ execute-info-setup-cmd.sh
@@ -1,10 +1,30 @@
+echo 'export MACHINE_NAME=vir-ubuntu-18-04-001' >> /var/lib/machines/vir-ubuntu-18-04-001/home/aine/.bashrc
+wait $!
+echo 'export MACHINE_IP=192.168.1.210' >> /var/lib/machines/vir-ubuntu-18-04-001/home/aine/.bashrc
+wait $!
 cp /home/aine/script-machines/systemd_nspawn-container-list.json /var/lib/machines/vir-ubuntu-18-04-001/usr/local/src/systemd_nspawn-container-list.json
+wait $!
+echo 'export MACHINE_NAME=vir-ubuntu-18-04-002' >> /var/lib/machines/vir-ubuntu-18-04-002/home/aine/.bashrc
+wait $!
+echo 'export MACHINE_IP=' >> /var/lib/machines/vir-ubuntu-18-04-002/home/aine/.bashrc
 wait $!
 cp /home/aine/script-machines/systemd_nspawn-container-list.json /var/lib/machines/vir-ubuntu-18-04-002/usr/local/src/systemd_nspawn-container-list.json
 wait $!
+echo 'export MACHINE_NAME=vir-ubuntu-18-04-003' >> /var/lib/machines/vir-ubuntu-18-04-003/home/aine/.bashrc
+wait $!
+echo 'export MACHINE_IP=192.168.1.212' >> /var/lib/machines/vir-ubuntu-18-04-003/home/aine/.bashrc
+wait $!
 cp /home/aine/script-machines/systemd_nspawn-container-list.json /var/lib/machines/vir-ubuntu-18-04-003/usr/local/src/systemd_nspawn-container-list.json
 wait $!
+echo 'export MACHINE_NAME=vir-ubuntu-18-04-004' >> /var/lib/machines/vir-ubuntu-18-04-004/home/aine/.bashrc
+wait $!
+echo 'export MACHINE_IP=192.168.1.213' >> /var/lib/machines/vir-ubuntu-18-04-004/home/aine/.bashrc
+wait $!
 cp /home/aine/script-machines/systemd_nspawn-container-list.json /var/lib/machines/vir-ubuntu-18-04-004/usr/local/src/systemd_nspawn-container-list.json
+wait $!
+echo 'export MACHINE_NAME=vir-ubuntu-18-04-005' >> /var/lib/machines/vir-ubuntu-18-04-005/home/aine/.bashrc
+wait $!
+echo 'export MACHINE_IP=192.168.1.214' >> /var/lib/machines/vir-ubuntu-18-04-005/home/aine/.bashrc
 wait $!
 cp /home/aine/script-machines/systemd_nspawn-container-list.json /var/lib/machines/vir-ubuntu-18-04-005/usr/local/src/systemd_nspawn-container-list.json
 wait $!
