732e456300ca490d8aaa776f76753f8a1221efaa
ukijumotahaneniarukenia <48931754+ukijumotahaneniarukenia@users.noreply.github.com>
2020-09-22 18:43:11 +0900

    Update create-info-setup-cmd.sh

--- create-info-setup-cmd.sh
+++ create-info-setup-cmd.sh
@@ -4,11 +4,9 @@ WORK_DIR=$HOME/script-machines
 
 ENV_DETERMINE_DIR=/usr/local/src
 
-ENV_HOME_DIR=/home/aine
-
-ENV_RC_FILE_NAME=.bashrc
-
 ENV_DETERMINE_FILE_NAME=systemd_nspawn-container-list.json
+
+ENV_VARIABLE_FILE_NAME=/etc/environment
 
 DEPLOY_DIR=/var/lib/machines
 
@@ -80,11 +78,11 @@ machinectl list -o json --no-pager | jq '' >$WORK_DIR/$ENV_DETERMINE_FILE_NAME
 eval echo {$START_HOST_NO..$END_HOST_NO} | xargs -n1|nl|\
   while read n h;do
 
-		echo -e "echo \x27export MACHINE_NAME=$REPLICA_NAME-$(printf  $SUBGRP_DIGIT $[n])\x27 >> $DEPLOY_DIR/$REPLICA_NAME-$(printf  $SUBGRP_DIGIT $[n])$ENV_HOME_DIR/$ENV_RC_FILE_NAME"
+    echo -e "echo \x27export MACHINE_NAME=$REPLICA_NAME-$(printf  $SUBGRP_DIGIT $[n])\x27 >> $DEPLOY_DIR/$REPLICA_NAME-$(printf  $SUBGRP_DIGIT $[n])$ENV_VARIABLE_FILE_NAME"
 
-		MACHINE_IP=$(cat $WORK_DIR/$ENV_DETERMINE_FILE_NAME | jq -r --arg MACHINE_NAME $REPLICA_NAME-$(printf  $SUBGRP_DIGIT $[n]) 'map(select(.machine==$MACHINE_NAME))[].addresses|split("\n")[0]')
+    MACHINE_IP=$(cat $WORK_DIR/$ENV_DETERMINE_FILE_NAME | jq -r --arg MACHINE_NAME $REPLICA_NAME-$(printf  $SUBGRP_DIGIT $[n]) 'map(select(.machine==$MACHINE_NAME))[].addresses|split("\n")[0]')
 
-		echo -e "echo \x27export MACHINE_IP=$MACHINE_IP\x27 >> $DEPLOY_DIR/$REPLICA_NAME-$(printf  $SUBGRP_DIGIT $[n])$ENV_HOME_DIR/$ENV_RC_FILE_NAME"
+    echo -e "echo \x27export MACHINE_IP=$MACHINE_IP\x27 >> $DEPLOY_DIR/$REPLICA_NAME-$(printf  $SUBGRP_DIGIT $[n])$ENV_VARIABLE_FILE_NAME"
 
     echo "cp $WORK_DIR/$ENV_DETERMINE_FILE_NAME $DEPLOY_DIR/$REPLICA_NAME-$(printf  $SUBGRP_DIGIT $[n])$ENV_DETERMINE_DIR/$ENV_DETERMINE_FILE_NAME"
 
